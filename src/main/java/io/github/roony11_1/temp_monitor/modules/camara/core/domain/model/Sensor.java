b352c9369c6e9c1889023d3edcd13f3f49bda31c
Roony11-1 <eltheasasin@gmail.com>
2026-07-31 00:34:18 -0400

    feat: Add one-to-many relationship for sensores in Sensor model; enhance data structure for better management

--- src/main/java/io/github/roony11_1/temp_monitor/modules/camara/core/domain/model/Sensor.java
+++ src/main/java/io/github/roony11_1/temp_monitor/modules/camara/core/domain/model/Sensor.java
@@ -1,6 +1,8 @@
 package io.github.roony11_1.temp_monitor.modules.camara.core.domain.model;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -14,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,6 +51,11 @@ public class Sensor
     @JoinColumn(name = "camara_id")
     @ToString.Exclude
     private Camara camara;
+
+    @OneToMany(mappedBy = "camara", fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @Builder.Default
+    private final List<Sensor> sensores = new ArrayList<>();
 
     private Instant ultimoContacto;
 
