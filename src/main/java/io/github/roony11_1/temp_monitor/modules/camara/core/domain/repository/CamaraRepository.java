a865787a697f882fc32744c9487185e5ab37246a
Roony11-1 <eltheasasin@gmail.com>
2026-08-01 01:49:05 -0400

    feat: Add EntityGraph support for Camara retrieval with specifications

--- src/main/java/io/github/roony11_1/temp_monitor/modules/camara/core/domain/repository/CamaraRepository.java
+++ src/main/java/io/github/roony11_1/temp_monitor/modules/camara/core/domain/repository/CamaraRepository.java
@@ -3,6 +3,8 @@ package io.github.roony11_1.temp_monitor.modules.camara.core.domain.repository;
 import io.github.roony11_1.temp_monitor.modules.camara.core.domain.model.Camara;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -13,6 +15,10 @@ public interface CamaraRepository extends JpaRepository<Camara, Long>, JpaSpecif
     List<Camara> findBySucursalId(Long sucursalId);
 
     Page<Camara> findBySucursalId(Long sucursalId, Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = "sucursal")
+    Page<Camara> findAll(Specification<Camara> spec, Pageable pageable);
 
     long countByActivo(boolean activo);
 }
